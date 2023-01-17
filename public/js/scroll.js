00ae25999e0a093aae894a5b5cc5a9695e576090
Wener Rodrigo <wener19.wr@gmail.com>
2023-01-17 20:31:13 -0300

    finalizado validações de api , estilo e rotas

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,11 +1,11 @@
 const setupScrolling = () => {
-    const container = [...document.querySelectorAll('.movie-container')]
+    const conainter = [...document.querySelectorAll('.movie-container')];
     const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
     const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-    container.forEach((item, i) => {
-        let containerDimension = item.getBoundingClientRect();
-        let containerWidth = containerDimension.width;
+    conainter.forEach((item, i) => {
+        let containerDimensions = item.getBoundingClientRect();
+        let containerWidth = containerDimensions.width;
 
         nxtBtn[i].addEventListener('click', () => {
             item.scrollLeft += containerWidth;
